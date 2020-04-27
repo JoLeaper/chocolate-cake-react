c1bcdc72f53dd26a78f42b14a6fcb84d8ea9a7a8
JoLeaper <joleap123@gmail.com>
2020-04-27 15:52:42 -0700

    added basic file structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Header from './Header/Header.js'
+import Body from './Body/Body.js'
+import Footer from './Footer/Footer.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Body />
+        <Footer />
+      </div>
+
+    )
+  }
 }
-
-export default App;
---
+++ src/Body/Body.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class Body extends Component {
+    render() {
+        return (
+            <div>
+                {/* <ArticleTitle />
+                <ArticleMetaInfo />
+                <ArticlePrintButton />
+                <ArticlePicture />
+                <ArticleText />
+                <IngredientList />
+                <BottomBorder />
+                <AuthorInfo />
+                <ShareButton /> */}
+            </div>
+        )
+    }
+}
