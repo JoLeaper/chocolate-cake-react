e4c32c8276a399e6b1338b1d1f473fca5fc76bd1
keegers <nikkikiga@gmail.com>
2020-04-27 17:48:37 -0700

    author and partial ingredients

--- src/Body/IngredientList.js
+++ src/Body/IngredientList.js
@@ -5,7 +5,14 @@ export default class IngredientList extends Component {
     render() {
         return (
             <div className="ingredients-list">
-                
+                <ul class="list-left">
+                    <li>1 1/2 cups milk</li>
+                    <li>1/2 cup marscaponek</li>
+                    <li>1/2 tsp pink salt</li>
+                    <li>1 lb Black Mision Figs</li>
+                    <li>1/2 cup brown sugar</li>
+                    <li>2-4 tbsp water</li>
+                </ul>
             </div>
         )
     }
