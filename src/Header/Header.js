bfab1352eff4ba7365756c0a8240901d7a78555e
JoLeaper <joleap123@gmail.com>
2020-04-27 17:41:54 -0700

    added text and spaced buttons

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react'
 import TitleSlogan from './TitleSlogan.js';
 import SocialMedia from './SocialMedia.js';
 import BottomBorder from './BottomBorder.js';
+import './Header.css';
 
 export default class Header extends Component {
     render() {
         return (
-            <div>
+        <div>
+            <div className='Header'>
                 <TitleSlogan />
                 <SocialMedia />
+            </div>
+            <div>
                 <BottomBorder />
             </div>
+        </div>
         )
     }
 }
--- src/Header/SocialMedia.js
+++ src/Header/SocialMedia.js
@@ -7,18 +7,27 @@ import instaIcon from '../lab-assets/insta-icon.png'
 import flicIcon from '../lab-assets/flic-icon.png'
 import pintIcon from '../lab-assets/pint-icon.png'
 
+import mailIcon from '../lab-assets/mail-icon.png'
+import rssIcon from '../lab-assets/rss-icon.png'
+
 
 
 export default class SocialMedia extends Component {
     render() {
         return (
-            <div>
-                <img alt='facebook icon button' src={fbIcon}></img>
-                <img alt='twitter icon button' src={twitIcon}></img>
-                <img alt='google plus icon button' src={gpIcon}></img>
-                <img alt='instagram icon button' src={instaIcon}></img>
-                <img alt='flic icon button' src={flicIcon}></img>
-                <img alt='pinterest icon button' src={pintIcon}></img>
+            <div className='button-container'>
+                <div className='social-media'>
+                    <img alt='facebook icon button' src={fbIcon}></img>
+                    <img alt='twitter icon button' src={twitIcon}></img>
+                    <img alt='google plus icon button' src={gpIcon}></img>
+                    <img alt='instagram icon button' src={instaIcon}></img>
+                    <img alt='flic icon button' src={flicIcon}></img>
+                    <img alt='pinterest icon button' src={pintIcon}></img>
+                </div>
+                <div className='sharing-button'>
+                    <img alt='mail icon button' src={mailIcon}></img>
+                    <img alt='rss icon button' src={rssIcon}></img>
+                </div>
             </div>
         )
     }
